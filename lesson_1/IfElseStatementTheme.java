d9cc81a4684d0f46f35de66f35e23df3f8b2cf1d
2Barbosa <fedorovba3@gmail.com>
2023-06-21 14:11:14 +0900

    Implement_HW2

---
+++ lesson_1/IfElseStatementTheme.java
@@ -0,0 +1,120 @@
+public class IfElseStatementTheme {
+    
+    public static void main(String[] args) {
+        System.out.println("1. Перевод псевдокода на язык Java");
+        int age = 30;
+        char sex = 11;
+        double height = 1.9;
+        String string = "Иван";
+        char firstLetterOfName = string.charAt(0);
+        if(age > 20) {
+            System.out.println("Вам можно");
+        } else {
+            System.out.println("Вам нельзя! Вы ещё не достигли 20 лет");
+        }
+        if (sex != 11) {
+            System.out.println(sex + "Вы женщина");
+        } else {
+            System.out.println(sex + " Вы мужчина");
+        }
+        if (height < 1.8) { // Пример 1,9
+            System.out.println("Вы низкого роста или ваш рост стандартный");
+        } else {
+            System.out.println("Вы высокий!");
+        }
+        if (firstLetterOfName == 'M') {
+            System.out.println("Ваше имя начинается с буквы M");
+        } else if (firstLetterOfName =='I') {
+            System.out.println("Ваше имя начинается с буквы M");
+        } else {
+            System.out.println("Выше имя начинается с любой другой буквы, кроме M и I.");
+        }
+
+        System.out.println("2. Поиск max и min числа");
+        int a = 12, b = 34;
+        if(a < b && a != b) {
+            System.out.println("max " + b + " min " + a);
+        a = a + b - (b = a);
+        if(a < b) {
+            System.out.println("max " + b + " min " + a);
+        } else {
+            System.out.println("нет, не верно");
+        } 
+
+        System.out.println("3. Проверка числа");
+        int sumInt = 100;
+        if(sumInt != 0 && sumInt % 2 == 0 && sumInt > 0) {
+            System.out.println("Число " + sumInt + " не является нулем," + 
+                "четное, не является отрицательным");
+        }
+
+        System.out.println("5. Определение символа по его коду");
+        char symbol = '\u0057';
+        if (symbol >= 'a' && symbol <= 'z') {
+            System.out.println(symbol + " - маленькая буква");
+        } else if (symbol >= 'A' && symbol <= 'Z') {
+            System.out.println(symbol + " - большая буква");
+        } else if (symbol >= '0' && symbol <= '9') {
+            System.out.println(symbol + " - число");
+        } else {
+            System.out.println(symbol + " - не буква и не число");
+        }
+
+        System.out.println("6. Подсчет суммы вклада");
+        double deposit = 300000; // депозит
+        double interestRate = 0; // процентная ставка
+        double interest = 0; // процент
+        double totalAmount = 0; // итоговая сумма
+        if (deposit < 100000) {
+            interestRate = 0.05;
+        } else if (deposit >= 100000 && deposit <= 300000) {
+            interestRate = 0.07;
+        } else if (deposit > 300000) {
+            interestRate = 0.1;
+        interest = deposit * interestRate; // вычисление процента
+        totalAmount = deposit + interest; // вычисление итоговой суммы
+        System.out.println("Сумма вклада: " + deposit + " руб.");
+        System.out.println("Начисленный %: " + interest + " руб.");
+        System.out.println("Итоговая сумма с %: " + totalAmount + " руб.");
+        } 
+
+        System.out.println("7. Определение оценки по предметам");
+        double history = 59;
+        double programming = 91;
+        int history_mark = (history > 60) ? ((history > 73) ? 4 : 3) : 2;
+        int programming_mark = (programming > 60) ? ((programming > 73) ? 4 : 3) : 2;
+        System.out.println("Оценка по предмету \"История\": " + history_mark);
+        System.out.println("Оценка по предмету \"Программирование\": " + programming_mark);
+        // вычисление средней оценки
+        double simply_mark = (history_mark + programming_mark) / 2.0;
+        System.out.println("Средний балл оценок по предметам: " + simply_mark);
+        // вычисление среднего процента по предметам
+        double symply_interest = (history + programming) / 2.0;
+        System.out.println("Средний % по предметам: " + symply_interest);
+        }
+
+        System.out.println("8 Расчет прибыли за год");
+        int rent = 5_000;
+        int sales = 13_000;
+        int costPrice = 9_000;
+        int months = 12;
+        if((sales - ((rent + costPrice)) * months) > 0) {
+            System.out.println("Прибыль за год " + (sales - (rent + costPrice)) * months + "руб.");
+        } else { 
+            System.out.println("Прибыли нет");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
